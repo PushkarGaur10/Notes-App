7bc06d44ff18400dd8843eb1c7b9bfe8c0b2725d
PushkarGaur10 <gaurpushkar10@gmail.com>
2022-11-24 14:07:57 +0530

    Updates

---
+++ precache-manifest.9981a463afd8f2b6684cbd4d2f059a4c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824e49bb2305f15cfc278d81af2db934",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "19d9d589a7977df351e7",
+    "url": "/notes-app/static/js/2.b271a242.chunk.js"
+  },
+  {
+    "revision": "630dddeb95d38a064ebb",
+    "url": "/notes-app/static/js/main.5b1f0a2f.chunk.js"
+  },
+  {
+    "revision": "1ab228e45428be91d417",
+    "url": "/notes-app/static/js/runtime-main.4d7ad299.js"
+  }
+]);
